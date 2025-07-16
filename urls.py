bd5f6c3e55dae5ad73ac8867426ee24392637a23
Umid780 <umidahmatov166@gmail.com>
2025-07-16 09:27:01 +0300

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('add/', views.add_product, name='add_product'),
+]
